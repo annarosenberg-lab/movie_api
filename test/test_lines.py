c535023773d9d3b8cc289af3758c6d42ef3cacd9
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-04-22 15:32:32 -0700

    Create test_lines.py

---
+++ test/test_lines.py
@@ -0,0 +1,47 @@
+from fastapi.testclient import TestClient
+
+from src.api.server import app
+
+import json
+
+client = TestClient(app)
+
+
+def test_get_line():
+    response = client.get("/lines/66")
+    assert response.status_code == 200
+
+    with open("test/lines/66.json", encoding="utf-8") as f:
+        assert response.json() == json.load(f)
+
+
+
+def test_404():
+    response = client.get("/lines/400")
+    assert response.status_code == 404
+
+def test_lines():
+    response = client.get("/lines/")
+    assert response.status_code == 200
+
+    with open("test/lines/root.json", encoding="utf-8") as f:
+        assert response.json() == json.load(f)
+
+def test_sort_filter():
+    response = client.get(
+        "/lines/?text=Why&movie_title=10&limit=50&offset=0&sort=character"
+    )
+    assert response.status_code == 200
+
+    with open(
+        "test/lines/filter.json",
+        encoding="utf-8",
+    ) as f:
+        assert response.json() == json.load(f)
+
+def test_get_conversation():
+    response = client.get("/conversations/0")
+    assert response.status_code == 200
+
+    with open("test/lines/0.json", encoding="utf-8") as f:
+        assert response.json() == json.load(f)
