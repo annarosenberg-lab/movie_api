dcfc1042688daa00f8105b52bd8d5e152d07a603
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-04-22 15:35:49 -0700

    Update server.py

--- src/api/server.py
+++ src/api/server.py
@@ -25,6 +25,10 @@ tags_metadata = [
         "name": "movies",
         "description": "Access information on top-rated movies.",
     },
+        {
+        "name": "lines",
+        "description": "Access information on lines in top-rated movies.",
+    }
 ]
 
 app = FastAPI(
@@ -32,14 +36,16 @@ app = FastAPI(
     description=description,
     version="0.0.1",
     contact={
-        "name": "Lucas Pierce",
-        "email": "lupierce@calpoly.edu",
+        "name": "Anna Rosenberg",
+        "email": "arosen12@calpoly.edu",
     },
     openapi_tags=tags_metadata,
 )
 app.include_router(characters.router)
 app.include_router(movies.router)
+app.include_router(lines.router)
 app.include_router(pkg_util.router)
+
 app.include_router(conversations.router)
 
 
