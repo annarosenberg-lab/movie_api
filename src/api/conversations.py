4edaab2c2c8172ce4fd03010136042ca36b478c2
Anna Rosenberg <58891612+annarosenberg-lab@users.noreply.github.com>
2023-04-24 16:03:30 -0700

    Update conversations.py

--- src/api/conversations.py
+++ src/api/conversations.py
@@ -59,18 +59,17 @@ def add_conversation(movie_id: int, conversation: ConversationJson):
     line_id = max(d.id for d in db.lines.values()) + 1
 
     for line in conversation.lines:
-        #db.lines[line_id] = db.Line(line_id, line.character_id, movie_id, conversation_id, line_sort, line.line_text)
+        db.lines[line_id] = db.Line(line_id, line.character_id, movie_id, conversation_id, line_sort, line.line_text)
         db.convo_lines.append({"line_id": line_id, "character_id": line.character_id, "movie_id": movie_id, "conversation_id": conversation_id, "line_sort": line_sort, "line_text": line.line_text})
         db.upload_new_lines()
         line_sort += 1
         line_id += 1
 
-    #db.conversations[conversation_id] = db.Conversation(conversation_id, character_1.id, character_2.id, movie_id, len(conversation.lines))
+    db.conversations[conversation_id] = db.Conversation(conversation_id, character_1.id, character_2.id, movie_id, len(conversation.lines))
     db.convos.append({"conversation_id": conversation_id, "character1_id": character_1.id, "character2_id": character_2.id, "movie_id": movie_id})
     db.upload_new_conversation()
 
     return {"conversation_id": conversation_id}
-   # return {"conversation_id": conversation_id}
 
     
   
